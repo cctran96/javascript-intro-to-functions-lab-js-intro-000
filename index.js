5264f326943fcd41956a60af9517fe3dff25d085
Chau Tran <cctran96@gmail.com>
2021-02-17 02:34:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toUpperCase()) {
+    return "YES INDEED!"
+  } elif (string === "I love you, Grandma." {
+    return "I love you, too."
+  } else {
+    return "I can't hear you!"
+  }
+}
